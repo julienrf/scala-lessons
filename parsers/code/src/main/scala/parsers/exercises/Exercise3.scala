a72803afb7e08fe7eeb6d7af8e1e90ac802550cb
Julien Richard-Foy <julien@richard-foy.fr>
2012-04-10 14:50:11 +0200

    Use by-name parameters

--- parsers/code/src/main/scala/parsers/exercises/Exercise3.scala
+++ parsers/code/src/main/scala/parsers/exercises/Exercise3.scala
@@ -4,6 +4,6 @@ import scala.util.parsing.combinator.JavaTokenParsers
 
 trait Exercise3 extends JavaTokenParsers {
 
-  def alt[A](p1: Parser[A], p2: Parser[A]): Parser[A] = sys.error("TODO")
+  def alt[A](p1: => Parser[A], p2: => Parser[A]): Parser[A] = sys.error("TODO")
 
 }
--- parsers/code/src/main/scala/parsers/solutions/Exercise3.scala
+++ parsers/code/src/main/scala/parsers/solutions/Exercise3.scala
@@ -4,7 +4,7 @@ import scala.util.parsing.combinator.JavaTokenParsers
 
 trait Exercise3 extends JavaTokenParsers {
 
-  def alt[A](p1: Parser[A], p2: Parser[A]) = Parser { in =>
+  def alt[A](p1: => Parser[A], p2: => Parser[A]) = Parser { in =>
     val res1 = p1(in)
     if (res1.successful) res1 else p2(in)
   }
