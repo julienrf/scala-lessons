3a387be2d81f70150e0300313eb6f638001f8b05
Julien Richard-Foy <julien@richard-foy.fr>
2013-04-28 20:21:45 +0200

    Hop

--- course/code/src/main/scala/julienrf/course/gameoflife/GameOfLife.scala
+++ course/code/src/main/scala/julienrf/course/gameoflife/GameOfLife.scala
@@ -1,22 +1,9 @@
 package julienrf.course.gameoflife
 
-import java.awt.event.{ActionEvent, ActionListener}
-
 object GameOfLife {
 
   def start(size: Int) {
-    val world = World.random(size, size * size / 8)
-    val ui = new Ui(size)
 
-    Events.every(150)
-    //ui.nextClicks
-      .fold(world)((_, world) => world.next)
-      .foreach { w =>
-        val cells = w.cells.fold(List.empty[(Int, Int)]){
-          case ((x, y, cell), cs) => if (cell.alive) (x, y) :: cs else cs
-        }
-        ui.updateWorld(cells)
-      }
   }
 
 }
